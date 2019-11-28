fff646b719b6a0c5938271a1be3cce77950f53eb
ArayCHN <1172094131@qq.com>
2019-11-27 17:38:38 -0800

    added pseudo env, not actual env

--- crosswalk/crosswalk/envs/crosswalk_env.py
+++ crosswalk/crosswalk/envs/crosswalk_env.py
@@ -1,18 +1,37 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+from gym import spaces
 
 class CrosswalkEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
+    super(CrosswalkEnv, self).__init__()
+    self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(1, 1), dtype=np.float32) # must be symmetric for ddpg
+    # observation: the closest 3 people, each 1.0 / distance_x, distance_y, (intensity), velocity (vx, vy)
+    # TODO specify observation
+    self.observation_space = spaces.Box(low=np.array([0.0, -1.0, -1.0, 0.0, -1.0, -1.0, 0.0, -1.0, -1.0]), 
+                                       high=np.array([10.0, 1.0, 1.0, 10.0, 1.0, 1.0, 10.0, 1.0, 1.0]), dtype=np.float32)
     print("initialized environment!")
-    pass
+
   def step(self, action):
-    pass
+    # TODO use actual steps
+    info = {} # dict, debug info
+    new_obs = np.array([0.0] * 9)
+    rew = 0.0
+    done = True
+    return new_obs, rew, done, info
+
   def reset(self):
-    pass
+    # TODO retrieve an experience from dataset and initialize the location of ego vehicle
+    obs = np.array([0.0] * 9)
+    return obs # initial observation
+
   def render(self, mode='human'):
+    # do not render anything
     pass
+
   def close(self):
+    # TODO what is this function?
     pass
